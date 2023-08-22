cd10ec2cb28e09e31ecd89af5a8c560bb6e838c2
mocfunky <mocfunky@gmail.com>
2023-08-22 18:16:12 -0500

    Snipers only server module
    Sets player to recon role and disables the ability to change to other roles. For sniper only server.

---
+++ Snipers.cs
@@ -0,0 +1,19 @@
+﻿using BattleBitAPI.Common;
+using BBRAPIModules;
+using System.Threading.Tasks;
+
+namespace Snipers
+{
+    public class Snipers : BattleBitModule
+    {
+        public override Task OnPlayerConnected(RunnerPlayer player)
+        {
+            player.SetNewRole(GameRole.Recon);
+            return Task.CompletedTask;
+        }
+        public override async Task<bool> OnPlayerRequestingToChangeRole(RunnerPlayer player, GameRole requestedRole)
+        {
+            return GameRole.Recon == requestedRole;
+        }
+    }
+}
