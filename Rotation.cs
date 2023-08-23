83fe7c3a77320f21643413e1c831e6cbbf725708
mocfunky <mocfunky@gmail.com>
2023-08-22 19:46:13 -0500

    Gamemode and Map rotations module
    This is basic settings for gamemode and map rotations. You can also use Add or Remove instead of Set

---
+++ Rotation.cs
@@ -0,0 +1,20 @@
+﻿using BBRAPIModules;
+using System.Threading.Tasks;
+
+namespace Rotations
+{
+    public class Rotation : BattleBitModule
+    {
+        public override Task OnConnected()
+        {
+            // The gamemodes you want
+
+            this.Server.GamemodeRotation.SetRotation("InfantryConquest");
+
+            // The maps you want
+
+            //this.Server.MapRotation.SetRotation("Valley");
+            return Task.CompletedTask;
+        }
+    }
+}
