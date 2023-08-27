1d5e3f0bdf00e26a2655cff0dfe4116e5eef3464
mocfunky <69504986+mocfunky@users.noreply.github.com>
2023-08-27 01:39:02 -0400

    Snipers Only Module
    This module sets all players to recon when they join and forces them to stay as recon only. It also disables night map voting.

--- Snipers.cs
+++ Snipers.cs
@@ -6,14 +6,29 @@ namespace Snipers
 {
     public class Snipers : BattleBitModule
     {
+        //
+        // Sets player's role to recon when they join
+        //
         public override Task OnPlayerConnected(RunnerPlayer player)
         {
             player.SetNewRole(GameRole.Recon);
             return Task.CompletedTask;
         }
+        //
+        // Only allows to choose recon role
+        //
         public override async Task<bool> OnPlayerRequestingToChangeRole(RunnerPlayer player, GameRole requestedRole)
         {
             return GameRole.Recon == requestedRole;
+        }
+        //
+        // Disables night voting
+        //
+        public override Task OnConnected()
+        {
+            this.Server.ServerSettings.CanVoteDay = true;
+            this.Server.ServerSettings.CanVoteNight = false;
+            return Task.CompletedTask;
         }
     }
 }
